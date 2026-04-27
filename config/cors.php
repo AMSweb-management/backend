1cbe473517b0ad75a22f7c24556695c91deb1db3
Novalardi21 <novalsantria320@gmail.com>
2026-04-27 14:44:53 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,26 +1,15 @@
 <?php
 
 return [
-
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://assetmanagementsystem.web.id',
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:5173',
+        'https://assetmanagementsystem.web.id'
     ],
-
-    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
